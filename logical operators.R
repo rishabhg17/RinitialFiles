2e5e72f1101b51b429dc980c633d6caa477c4a09
rishabhg17 <g.rishabh13@gmail.com>
2020-10-11 13:46:48 +0530

    R files

---
+++ logical operators.R
@@ -0,0 +1,29 @@
+#logical:
+#true T
+#false F
+
+4<5
+10>100
+4==5
+# ==
+# !=
+# <
+# >
+# <=
+# >=
+# !
+# |
+# &
+# isTrue(x)
+
+result <- 4 < 5
+result
+typeof(result)
+
+result2 <- !(2>1)
+result2
+
+result | result2
+result & result2
+
+isTRUE(result)

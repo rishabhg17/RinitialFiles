a50136007395e0ec2b48f6ae984c37bbb3ea5fce
rishabhg17 <g.rishabh13@gmail.com>
2020-10-11 13:51:16 +0530

    R files

---
+++ matrixfunctions.R
@@ -0,0 +1,25 @@
+#matrix
+
+my.data<- 1:20
+my.data
+
+A <- matrix(my.data,4,5)
+A
+A[2,3]
+B<- matrix(my.data,4,5, byrow=TRUE)
+B
+B[2,5]
+
+#rbind()
+
+r1<-c("I","am","happy")
+r2<-c("what","a","day")
+r3<-c(1,2,3)
+a<-rbind(r1,r2,r3)
+a
+
+#cbind
+c1<- 1:5
+c2<- -1:-5
+d<- cbind(c1,c2)
+d

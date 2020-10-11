fe6af4524478b8e173b1e1188bea490e5391cde3
rishabhg17 <g.rishabh13@gmail.com>
2020-10-11 13:28:20 +0530

    R files

---
+++ first file.R
@@ -0,0 +1,27 @@
+
+
+#integer
+
+x <- 2L
+typeof(x)
+
+#double
+y <- 2.5
+typeof(y)
+
+
+#complex
+
+z <- 3 + 2i
+typeof(z)
+
+#character
+
+a <- "H"
+typeof(a)
+
+
+#logical
+ q <- T
+ typeof(q)
+ 
